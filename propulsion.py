f27b77bd0cf376d6a52f43d83f15465841f122ee
FryerWire <maxwell.seery@gmail.com>
2025-03-05 17:27:52 -0600

    Created this file as the main program runner.

--- propulsion.py
+++ propulsion.py
@@ -10,8 +10,30 @@ from fanno_flow import fanno_flow_solver as ffc
 import rayleigh_flow as rfc
 import isentropic_flow as ifc
 import normal_shocks as nsc
+from turbojet import turbo_jet_solver as tjc
+from variable_mapping import variable_mapping as map
 
 
 
 # Main Program ====================================================================================
-print(ffc("M", 7))
+gas_parameters = {'R': 287, 'g': 1.4, 'cp': 1004}
+knowns = {"M0": 2.5, "P0": 15000, "T0": 273.15 - 40, "Tt4": 1800, 'h': 42000000, "pi_c": 14}
+
+result = tjc(knowns, gas_parameters)
+
+
+
+# Fancy Printer ===================================================================================
+full_data = dict(map(result)) 
+for main_category, sub_dict in full_data.items():
+    print(f"\n{main_category}:")
+    
+    for sub_category, values in sub_dict.items():
+        if isinstance(values, dict):  
+            print(f"\n  {sub_category}:")
+            
+            for key, value in values.items():
+                print(f"    {key}: {value}")
+                
+        else: 
+            print(f"  {sub_category}: {values}")
