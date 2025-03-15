10ce2405d341de977268829340f3dd7cad83a779
FryerWire <maxwell.seery@gmail.com>
2025-03-14 22:18:05 -0500

    Main: Changed the imports style Created: Unit Converter

---
+++ Utilities/unit_converter.py
@@ -0,0 +1,92 @@
+
+"""
+Unit Converter
+Start Date        : 3/14/2025
+Modification Date : 3/14/2025
+"""
+
+
+
+def unit_conversion(value, from_unit, to_unit):
+    """
+    Converters to any unit. 
+    
+    Parameters:
+    - value (float)   : Any float value
+    - from_unit (str) : Any string in SI_prefix, imperial_units, temp_to_kelvin, and kelvin_to_temp
+    - to_unit (str)   : Any string in SI_prefix, imperial_units, temp_to_kelvin, and kelvin_to_temp
+    
+    Returns:
+    - converted_value (float) : The new unit converted value
+    
+    Example:
+    >>> print(unit_conversion(19, 'C', 'R'))
+    """
+    
+    # Metric --------------------------------------------------------------------------------------
+    SI_prefix = {
+        'T'  : 1e+12, # Tera
+        'G'  : 1e+9,  # Giga
+        'M'  : 1e+6,  # Mega
+        'k'  : 1e+3,  # Kilo
+        'h'  : 1e+2,  # Hecto
+        'da' : 1e+1,  # Deca
+        'BU' : 1,     # Base Unit
+        'd'  : 1e-1,  # Deci
+        'c'  : 1e-2,  # Centi
+        'm'  : 1e-3,  # Milli
+        'u'  : 1e-6,  # Micro
+        'n'  : 1e-9,  # Nano
+        'p'  : 1e-12  # Pico
+    }
+
+    # Imperial Unit Values ------------------------------------------------------------------------
+    imperial_units = {
+        'inch'  : 0.0254,    # Inch
+        'foot'  : 0.3048,    # Foot
+        'yard'  : 0.9144,    # Yard
+        'mile'  : 1609.34,   # Mile
+        'ounce' : 0.0283495, # Ounce
+        'pound' : 0.453592,  # Pound
+    }
+
+    # Temperature Conversion ----------------------------------------------------------------------
+    temp_to_kelvin = {
+        'C' : lambda t : t + 273.15,                  # Celsius
+        'F' : lambda t : (t - 32) * (5 / 9) + 273.15, # Fahrenheit
+        'R' : lambda t : t * (5 / 9),                 # Rankine
+        'K' : lambda t : t                            # Kelvin
+    }
+
+    kelvin_to_temp = {
+        'C' : lambda t : t - 273.15,                  # Celsius
+        'F' : lambda t : (t - 273.15) * (9 / 5) + 32, # Fahrenheit
+        'R' : lambda t : t * (9 / 5),                 # Rankine
+        'K' : lambda t : t                            # Kelvin
+    }
+    
+    
+    # Temperature Checks --------------------------------------------------------------------------
+    if (from_unit in temp_to_kelvin) and (to_unit in kelvin_to_temp):
+        value_in_kelvin = temp_to_kelvin[from_unit](value)
+        return kelvin_to_temp[to_unit](value_in_kelvin)
+
+    # 'to' and 'from' Unit Checks -----------------------------------------------------------------
+    if (from_unit in SI_prefix):
+        from_factor = SI_prefix[from_unit]
+    elif (from_unit in imperial_units):
+        from_factor = imperial_units[from_unit]
+    else:
+        raise TypeError("Invalid 'from' Unit")
+
+    if (to_unit in SI_prefix):
+        to_factor = SI_prefix[to_unit]
+    elif (to_unit in imperial_units):
+        to_factor = imperial_units[to_unit]
+    else:
+        raise TypeError("Invalid 'from' Unit")
+
+    value_in_base_unit = value * from_factor
+    converted_value = value_in_base_unit / to_factor
+
+    return converted_value
--- main.py
+++ main.py
@@ -1,51 +1,24 @@
 
 """
-Fanno Flow Calculator
+Main Program
 Start Date        : 3/4/2025
-Modification Date : 3/4/2025
+Modification Date : 3/14/2025
 """
 
 
-from fanno_flow import fanno_flow_solver as ffc
-import rayleigh_flow as rfc
-import isentropic_flow as ifc
-import normal_shocks as nsc
-from turbojet import turbojet_solver as tjc
-from Utilities.variable_mapping import variable_mapping as map
 
+# Local Imports ===================================================================================
+# Flow Solvers File -------------------------------------------------------------------------------
+from Flow_Solvers.fanno_flow import fanno_flow_solver as ffc
+from Flow_Solvers.isentropic_flow import iscentropic_flow_solver as ifs
+from Flow_Solvers.normal_shocks import normal_shock_solver as nss
+from Flow_Solvers.rayleigh_flow import rayleigh_flow_solver as rfs
+
+# External Imports ================================================================================
 import numpy as np
+
+
 
 # Main Program ====================================================================================
 gas_parameters = {'R': 287, 'g': 1.4, 'cp': 1004}
 
-# Knowns ------------------------------------------------------------------------------------------
-# knowns = {'M0': 2, 'P0': 10000, 'T0': 273.15 - 45, 'pi_c': 12, 'tau_lambda': 8, 'tau_t': 0.735}
-# knowns = {'M0': 1.5, 'P0': 25000, 'T0': 223, 'Tt4': 1900, 'h': 42800000}
-# knowns = {'Tt4': 1200, 'T0': 261, 'M0': 0.6, 'tau_c': 2}
-# knowns = {'M0': 2, 'pi_c': 9, 'T0': 200, 'Tt4': 1200}
-# knowns = {'M0': 2.5, 'P0': 15000, 'T0': 273.15 - 40, 'pi_c': 14, 'Tt4': 1800, 'h': 42000000}
-# knowns = {'M0': 0.4, 'P0': 95000, 'T0': 283.15, 'm0_dot': 65, 'pi_c': 25, 'tau_lambda': 6, 'h': 428000000}
-
-M0 = 222.222 / np.sqrt(1.4 * 287 * 223.15)
-
-knowns = {'M0': M0, 'T0': 223.15, 'P0': 24000, 'Tt4': 1093.15, 'h': 43300000, 'A': 0.08}
-
-# knowns = {'F': 50000, 'm0_dot': 45, 'mf_dot': 2.65, 'h':42800000, 'M0': 0.8, 'T0': 223}
-result = tjc(knowns)
-
-
-
-# Fancy Printer ===================================================================================
-full_data = dict(map(result)) 
-for main_category, sub_dict in full_data.items():
-    print(f"\n{main_category}:")
-    
-    for sub_category, values in sub_dict.items():
-        if isinstance(values, dict):  
-            print(f"\n  {sub_category}:")
-            
-            for key, value in values.items():
-                print(f"    {key}: {value}")
-                
-        else: 
-            print(f"  {sub_category}: {values}")
