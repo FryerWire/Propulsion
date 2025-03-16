cbff71b316cb9ac47ccc6500e98d1fa9ecb41e3f
FryerWire <maxwell.seery@gmail.com>
2025-03-16 17:06:05 -0500

    Recreated the answers file.  Modified the printing style of the answer checking file. Recreated the fancy printer to have styles of 2 and 3. Still need to do the last stile.

---
+++ Problem_Solvers/answers.py
@@ -0,0 +1,61 @@
+
+"""
+Class Answers
+Start Date        : 3/16/2025
+Modification Date : 3/16/2025
+"""
+
+
+
+# Local Imports ===================================================================================
+# Utilities ---------------------------------------------------------------------------------------
+from Utilities.unit_converter import unit_conversion as units
+
+
+# External Imports ================================================================================
+import numpy as np 
+
+
+
+# Lecture Solutions ===============================================================================
+L3_SOL = {
+    'Sections' : [
+        {'Section Num': 1, 'Flow Type': 'Isentropic', 'V1': 240, 'P1': 170000, 'T1': 320, 'M1': 0.67, 'Tt1': 349, 'Pt1': 230000},
+        {'Section Num': 2, 'Flow Type': 'Isentropic', 'V2': 290, 'P2': 170000, 'Tt2': 349, 'T2': 307, 'Pt2': 211000}
+    ]
+}
+
+L4_SOL = {
+    'Sections' : [
+        {'Section Num': 0, 'Flow Type': 'Isentropic', 'M0': 1.8, 'P0': units(0.3, 'atm', 'Pa'), 'T0': 250, 'Pt0': units(1.724, 'atm', 'Pa'), 'Tt0': 412},
+        {'Section Num': 1, 'Flow Type': 'Normal', 'M1': 0.6165, 'Pt1': units(1.4, 'atm', 'Pa')},
+        {'Section Num': 2, 'Flow Type': 'Isentropic', 'Pt2': units(1.4, 'atm', 'Pa'), 'Tt2': 412, 'M2': 0.168, 'P2': units(1.373, 'atm', 'Pa'), 'T2': 410}
+    ]
+}
+
+
+
+# Quiz Solutions ==================================================================================
+Q1_SOL = {
+    'Sections' : [
+        {'Section Num': 1, 'Flow Type': 'Isentropic', 'T1': 288, 'P1': 100000, 'M1': 0.3, 'Tt1': 273.5},
+        {'Section Num': 2, 'Flow Type': 'Isentropic', 'M2': 0.6, 'Tt2': 273.5, 'P2': 48700}
+    ]
+}
+
+
+
+# Testing Solutions ===============================================================================
+TEST_1 = {
+    'Sections' : [
+        {'Section Num': 1, 'Flow Type': 'Isentropic', 'V1': 240, 'P1': 170000, 'T1': 320, 'M1': 0.67, 'Tt1': 349, 'Pt1': 230000},
+        {'Section Num': 2, 'Flow Type': 'Isentropic', 'V2': 290, 'P2': 170000, 'Tt2': 349, 'T2': 307, 'Pt2': 211000}
+    ]
+}
+
+TEST_2 = {
+    'Sections' : [
+        {'Section Num': 1, 'Flow Type': 'Isentropic', 'V1': 480, 'P1': 170000, 'T1': 320, 'M1': 0.67, 'Tt1': 349, 'Pt1': 738},
+        {'Section Num': 2, 'Flow Type': 'Isentropic', 'V2': 290, 'P2': 180, 'Tt2': 349, 'T2': 307, 'Pt2': 211000, 'Z': 10}
+    ]
+}
